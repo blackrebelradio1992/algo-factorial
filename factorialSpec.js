8bdbc1c259c9144b85f847ddd8115a9240797d20
Benjamin Cohen <benjamin@codeplatoon.org>
2023-08-24 11:59:56 -0600

    removed last test as too finicky

--- factorialSpec.js
+++ factorialSpec.js
@@ -6,7 +6,4 @@ console.log(factorial(2) === 2);
 console.log(factorial(4) === 24);
 console.log(factorial(8) === 40320);
 console.log(factorial(18) === 6402373705728000);
-console.log(
-  factorial(45) === 119622220865480194561963161495657715064383733760000000000
-);
 // Test how high of a number your program can calculate. Can you push it further?
