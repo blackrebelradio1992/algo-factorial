38d8ed0b3bff09e58ea73e9b8f0f6fe78637be40
Frank_Gary <blackrebelradio1992@gmail.com>
2023-10-05 08:41:44 -0500

    updated results

--- factorial.js
+++ factorial.js
@@ -1,5 +1,15 @@
 function factorial(num) {
-  return 0;
+  let result = num;
+  if (num === 1 || num === 0) {
+    return 1
+  } 
+  while(num > 1){
+    num--
+    result = result * num
+  }
+  return result;
 }
+
+// console.log(factorial(4))
 
 module.exports = factorial;
